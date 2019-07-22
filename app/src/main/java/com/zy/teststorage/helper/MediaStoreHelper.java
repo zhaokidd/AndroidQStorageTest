8592e569f6bb832a24c50435f5f60b2f6415f217
zhaoyang <1011509513@qq.com>
2019-07-23 02:25:57 +0800

    temp

--- app/src/main/java/com/zy/teststorage/helper/MediaStoreHelper.java
+++ app/src/main/java/com/zy/teststorage/helper/MediaStoreHelper.java
@@ -1,4 +1,9 @@
 package com.zy.teststorage.helper;
 
+import android.provider.MediaStore;
+
 public class MediaStoreHelper {
+    public void test(){
+//        MediaStore.Video.Media.EXTERNAL_CONTENT_URI.buildUpon()
+    }
 }
